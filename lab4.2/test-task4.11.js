132f261342be574a9c08eefc0e1d6cce23f218c4
Mironova Olesya <dggdhdufiiehw524619485738@vk.com>
2026-02-17 13:40:50 +0900

    Добавила задание 4 НОВОЕ  по JavaScript

--- lab4.2/test-task4.11.js
+++ lab4.2/test-task4.11.js
@@ -1,25 +1,22 @@
 const peopleWaiting = ['Кристина', 'Олег', 'Кирилл', 'Мария', 'Светлана', 'Артем', 'Глеб'];
 
-// Функция для выдачи посылки (удаление из начала массива)
 function giveParcel() {
   const client = peopleWaiting.shift();
   alert(`${client} получил(а) посылку. В очереди осталось ${peopleWaiting.length} человек.`);
 }
 
-// Функция для ухода без посылки (удаление из конца массива)
+
 function leaveQueueWithoutParcel() {
   const client = peopleWaiting.pop();
   alert(`${client} не получил(а) посылку и ушел(ла) из очереди`);
 }
 
-// 1. Кристина и Олег получили посылки
-giveParcel(); // Кристина получила посылку
-giveParcel(); // Олег получил посылку
+giveParcel();
+giveParcel();
 
-// 2. Кирилл получает посылку, остальные уходят
-giveParcel(); // Кирилл получил посылку
+giveParcel();
 
-// Удаляем оставшихся людей, которые не получили посылки
+
 while (peopleWaiting.length > 0) {
   leaveQueueWithoutParcel();
 }
